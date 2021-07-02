9bc4b08a759668b4c085c40efbdb54c0458b6d91
Marc Forrer <marc.forrer@bluewin.ch>
2021-07-02 19:00:29 +0200

    implemented very basic AI movement

---
+++ Assets/Scripts/AITankInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AITankInput : MonoBehaviour {
+
+  [SerializeField] private List<Transform> _stops;
+  [SerializeField] private float _movementSpeed;
+  [SerializeField] private float _distanceDelta;
+  private Transform _transform;
+  private Transform _nextStop;
+  private int _stopIndex;
+
+  private Transform GetNextStop() {
+    if (_stopIndex >= _stops.Count) {
+      _stopIndex = 0;
+    }
+    return _stops[_stopIndex++];
+  }
+
+  private bool HasReachedStop() {
+    var direction = _nextStop.position - _transform.position;
+    return direction.magnitude <= _distanceDelta;
+  }
+
+  private void Awake() {
+    _transform = GetComponent<Transform>();
+  }
+
+  private void Start() {
+    _nextStop = GetNextStop();
+  }
+
+  private void FixedUpdate() {
+    if (HasReachedStop()) {
+      _nextStop = GetNextStop();
+    }
+    var direction = (_nextStop.position - _transform.position).normalized;
+    _transform.Translate(Time.fixedDeltaTime * _movementSpeed * direction);
+  }
+
+}
