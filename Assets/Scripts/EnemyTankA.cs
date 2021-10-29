9affb11745bf40504dd3c01408264cdac0279c5e
Marc Forrer <marc.forrer@bluewin.ch>
2021-10-29 23:24:43 +0200

    added basic behaviour to the enemy AI for shooting periodically

--- Assets/Scripts/EnemyTankA.cs
+++ Assets/Scripts/EnemyTankA.cs
@@ -1,13 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyTankA : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed;
     [SerializeField] private GameObject head;
+    [SerializeField] private ObjectSpawner bulletSpawner;
+    [SerializeField] private float heatUpTime;
+    [SerializeField] private float coolDownTime;
     private TankVision _tankVision;
     private FlipFlopRotator _rotator;
     private Transform _headTransform;
+    private Coroutine _shootingCoroutine;
 
+    private IEnumerator Shoot()
+    {
+        yield return new WaitForSeconds(heatUpTime);
+        bulletSpawner.Spawn();
+        yield return new WaitForSeconds(coolDownTime);
+        _shootingCoroutine = null;
+    }
+    
     private void Awake()
     {
         _tankVision = GetComponentInChildren<TankVision>();
@@ -29,8 +42,15 @@ public class EnemyTankA : MonoBehaviour
             }
             var currentDirection = _headTransform.right;
             var targetDirection = _tankVision.GetBestTargetDirection();
-            var directionDelta = targetDirection - currentDirection;
-            _headTransform.right = Vector3.MoveTowards(currentDirection, currentDirection + directionDelta, rotationSpeed);
+
+            if (Vector2.Distance(currentDirection.normalized, targetDirection.normalized) < 0.02f)
+            {
+                _shootingCoroutine ??= StartCoroutine(Shoot());
+                return;
+            }
+            
+            var expectedDirection = Vector3.MoveTowards(currentDirection, targetDirection, rotationSpeed);
+            _headTransform.right = expectedDirection;
         }
         else
         {
