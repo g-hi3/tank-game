358a12f9c3f6e96d1b66dea97e0c35e8de0e8fcd
Marc Forrer <marc.forrer@bluewin.ch>
2021-12-28 01:03:44 +0100

    added a confetti effect that activates when the player defeats all enemy tanks

---
+++ Assets/Scripts/ConfettiController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ConfettiController : MonoBehaviour
+{
+    private ParticleSystem _particles;
+
+    private void Awake()
+    {
+        _particles = GetComponent<ParticleSystem>();
+    }
+
+    private void OnGameOver()
+    {
+        _particles.Play();
+    }
+}
