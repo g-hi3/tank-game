56f390be5f4b481dacc26d580195f9c95b53fbd9
Marc Forrer <marc.forrer@bluewin.ch>
2021-12-31 03:09:43 +0100

    added new level with tank that moves in a set path

---
+++ Assets/Scripts/EnemyTankB.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTankB : MonoBehaviour
+{
+    [SerializeField] private List<Transform> path;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float distanceEpsilon;
+    private Transform _transform;
+    private Transform _currentNode;
+
+    private void Awake()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if (path.Count == 0)
+        {
+            return;
+        }
+        
+        if (_currentNode == null)
+        {
+            _currentNode = path[0];
+        }
+
+        if (Vector2.Distance(_transform.position, _currentNode.position) < distanceEpsilon)
+        {
+            var currentPathIndex = path.IndexOf(_currentNode) + 1;
+            _currentNode = currentPathIndex < path.Count ? path[currentPathIndex] : path[0];
+        }
+
+        _transform.position = Vector3.MoveTowards(_transform.position, _currentNode.position, movementSpeed * Time.deltaTime);
+    }
+}
