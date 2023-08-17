b3029b61e3da29ea3066f487fd6836b7eb620334
g-hi3 <marc.forrer@bluewin.ch>
2023-08-17 20:46:46 +0200

    added main menu and best times

---
+++ Assets/Scripts/UI/BestTimesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace TankGame.UI
+{
+    public class BestTimesViewModel : MonoBehaviour
+    {
+        [field: SerializeField] public UIDocument DocumentRoot { get; private set; }
+        [field: SerializeField] public string MainMenuSceneName { get; private set; }
+
+        private void OnMainMenuButtonClicked()
+        {
+            SceneManager.LoadSceneAsync(MainMenuSceneName);
+        }
+
+        private void Awake()
+        {
+            DocumentRoot = GetComponent<UIDocument>();
+        }
+
+        private void Start()
+        {
+            var mainMenuButton = DocumentRoot!.rootVisualElement.Q<Button>("MainMenuButton")!;
+            mainMenuButton.clicked += OnMainMenuButtonClicked;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuViewModel.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace TankGame.UI
+{
+    public class MainMenuViewModel : MonoBehaviour
+    {
+        [field: SerializeField] public UIDocument DocumentRoot { get; private set; }
+        [field: SerializeField] public string Level1SceneName { get; private set; }
+        [field: SerializeField] public string BestTimesSceneName { get; private set; }
+
+        private void OnStartGameButtonClicked()
+        {
+            SceneManager.LoadSceneAsync(Level1SceneName);
+        }
+
+        private void OnBestTimesButtonClicked()
+        {
+            SceneManager.LoadSceneAsync(BestTimesSceneName);
+        }
+
+        private static void OnQuitButtonClicked()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+        private void Awake()
+        {
+            DocumentRoot = GetComponent<UIDocument>();
+        }
+
+        private void Start()
+        {
+            var startGameButton = DocumentRoot!.rootVisualElement.Q<Button>("StartGameButton")!;
+            startGameButton.clicked += OnStartGameButtonClicked;
+
+            var bestTimesButton = DocumentRoot.rootVisualElement.Q<Button>("BestTimesButton")!;
+            bestTimesButton.clicked += OnBestTimesButtonClicked;
+
+            var quitButton = DocumentRoot.rootVisualElement.Q<Button>("QuitButton")!;
+            quitButton.clicked += OnQuitButtonClicked;
+        }
+    }
+}
